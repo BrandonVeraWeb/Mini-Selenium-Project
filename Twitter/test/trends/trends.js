edc2008631cf2d329ebad0f34eea7f3d32949264
Brandon Vera <108695519+BrandonVeraWeb@users.noreply.github.com>
2023-06-07 09:29:13 -0400

    test: trends finished (#2)

--- Twitter/test/trends/trends.js
+++ Twitter/test/trends/trends.js
@@ -3,7 +3,9 @@ const { driver } = require("../../src/driver");
 
 class Trends {
   async TrendsProcess() {
-    await clickTrends();
+    await this.clickTrends();
+    await this.clickLatest();
+    await this.clickProfile();
   }
 
   async clickTrends() {
@@ -16,6 +18,24 @@ class Trends {
       9000
     );
     await divTrends.click();
+  }
+  async clickLatest() {
+    let divLatest = await driver.wait(
+      until.elementLocated(By.linkText("View all")),
+      10000
+    );
+    await divLatest.click();
+  }
+  async clickProfile() {
+    let profileClick = await driver.wait(
+      until.elementLocated(
+        By.className(
+          "css-901oao r-1nao33i r-37j5jr r-a023e6 r-16dba41 r-rjixqe r-bcqeeo r-1h8ys4a r-1jeg54m r-qvutc0"
+        )
+      ),
+      12000
+    );
+    await profileClick.click();
   }
 }
 module.exports = Trends;
