ae91454490c3e9eaeccca92aa8eccc9c3a81b512
Brandon Vera <brandonveraweb@gmail.com>
2023-06-07 16:19:07 -0400

    perf: validation

--- LoginPage/utils/LoginPage.js
+++ LoginPage/utils/LoginPage.js
@@ -1,30 +1,20 @@
-const { Builder, By } = require("selenium-webdriver");
+const { By, Key, until } = require("selenium-webdriver");
+const { driver } = require("../../Twitter/src/driver");
 const assert = require("assert");
-let driver;
-driver = new Builder().forBrowser("chrome").build();
 class LoginPage {
-  constructor() {
-    this.emailField = driver.findElement(By.id("email"));
-    this.passwordField = driver.findElement(By.id("pass"));
-    this.page = driver.get("https://facebook.com");
-    this.click = driver.findElement(By.name("login"));
-  }
-
-  async OpenTwitter() {
-    await this.page;
-    await driver.manage().window().setRect({ width: 1624, height: 1268 });
+  async OpenFacebook() {
+    await driver.get("https://facebook.com");
+    await driver.manage().window().maximize();
   }
 
   async LoginSuccess() {
     await this.email("waxexa1154@hrisland.com");
     await this.password("carroSeguro23");
-    await this.click.click();
   }
 
   async LoginError() {
     await this.email("waxeca1154@hrisland.com");
     await this.password("123456789");
-    await this.click.click();
   }
 
   async email(email) {
@@ -35,6 +25,44 @@ class LoginPage {
   async password(password) {
     let passwordField = await driver.findElement(By.id("pass"));
     await passwordField.sendKeys(password);
+    await passwordField.sendKeys(Key.ENTER);
+  }
+
+  async validation() {
+    await this.navBarProfile();
+    await this.logoFacebook();
+    await this.numberPhone();
+  }
+
+  async navBarProfile() {
+    let profile = await driver.wait(
+      until.elementLocated(
+        By.className("x1lliihq x6ikm8r x10wlt62 x1n2onr6 xlyipyv xuxw1ft")
+      ),
+      5000
+    );
+    const text = await profile.getText();
+    assert(text === "Manuel");
+  }
+
+  async logoFacebook() {
+    let logo = await driver.wait(
+      until.elementLocated(By.className("x1lliihq x1k90msu x2h7rmj x1qfuztq")),
+      5000
+    );
+    assert(logo !== null);
+  }
+  async numberPhone() {
+    let divPhone = await driver.wait(
+      until.elementLocated(
+        By.className(
+          "x193iq5w xeuugli x13faqbe x1vvkbs x1xmvt09 x1lliihq x1s928wv xhkezso x1gmr53x x1cpjm7i x1fgarty x1943h6x x14z4hjw x3x7a5m xngnso2 x1qb5hxa x1xlr1w8 xzsf02u x1yc453h"
+        )
+      ),
+      5000
+    );
+    const text = await divPhone.getText();
+    assert(text === "Introduce un número de móvil");
   }
 
   async closeBrowser() {
