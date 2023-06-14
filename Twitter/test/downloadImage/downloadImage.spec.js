d1f9006eb0b8723a20f68f480e0a4a15c876af77
Brandon Vera <brandonveraweb@gmail.com>
2023-06-14 12:25:35 -0400

    perf: add validation downloadimg

--- Twitter/test/downloadImage/downloadImage.spec.js
+++ Twitter/test/downloadImage/downloadImage.spec.js
@@ -17,39 +17,3 @@ describe("Download Profile Picture", function (env) {
     await downloadImg.DownloadProcess();
   });
 });
-// const { By, until, Key } = require("selenium-webdriver");
-// const { driver } = require("../../src/driver");
-
-// class DownloadImg {
-//   async DownloadProcess() {
-//     await this.clickPicture();
-//     await this.downloadPicture();
-//   }
-
-//   async clickPicture() {
-//     let picture = await driver.wait(
-//       until.elementLocated(
-//         By.className(
-//           "css-1dbjc4n r-172uzmj r-1pi2tsx r-1ny4l3l r-o7ynqc r-6416eg r-13qz1uu"
-//         )
-//       ),
-//       8000
-//     );
-//     await picture.click();
-//   }
-
-//   async downloadPicture() {
-//     let pictureProfile = await driver.wait(
-//       until.elementLocated(By.className("css-9pa8cd")),
-//       12000
-//     );
-//     const actions = driver.actions({ async: true });
-//     await actions
-//       .contextClick(pictureProfile)
-//       .sendKeys(Key.ARROW_DOWN)
-//       .perform();
-//     // await driver.actions().sendKeys(Key.ARROW_DOWN).perform();
-//   }
-// }
-
-// module.exports = DownloadImg;
