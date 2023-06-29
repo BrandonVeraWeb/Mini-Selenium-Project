c8ba7f71e4c0e7e07615ceafec4923a912d6f919
Brandon Vera <brandonveraweb@gmail.com>
2023-06-29 15:04:36 -0400

    perf: Login

--- Twitter/utils/LogIn.js
+++ Twitter/utils/LogIn.js
@@ -1,16 +1,49 @@
 const { By, until, Key } = require("selenium-webdriver");
 const { driver } = require("../src/driver");
-
-class LoginPage {
-  async LoginProcess() {
-    await this.clickLog();
+const assert = require("assert");
+class Login {
+  async Success() {
     await this.loginEmail("@Manueltest48284");
     await this.passwordInput("carroSeguro23");
+    // await this.assert("https://twitter.com/home");
   }
+  async Error() {
+    await this.loginEmail("@Manueltest48284");
+    await this.passwordInput("passwordIncorrect");
+    await this.assert("https://twitter.com/i/flow/login");
+  }
+  async assert(pageUrl) {
+    let url = await driver.getCurrentUrl();
+    assert.equal(url, pageUrl);
+  }
+  async Logout() {
+    await this.Success();
+    let buttonProfile = await driver.wait(
+      until.elementLocated(
+        By.className(
+          "r-1nao33i r-4qtqp9 r-yyyyoo r-1xvli5t r-dnmrzs r-bnwqim r-1plcrui r-lrvibr"
+        )
+      ),
+      10000
+    );
+    await buttonProfile.click();
+    let selectLogout = await driver.wait(
+      until.elementLocated(
+        By.css('a[data-testid*="AccountSwitcher_Logout_Button"]')
+      ),
+      12000
+    );
+    await selectLogout.click();
 
-  async clickLog() {
-    const logIn = driver.wait(until.elementLocated(By.css('a[href="/login"]')));
-    await logIn.click();
+    let confirm = await driver.wait(
+      until.elementLocated(
+        By.css('div[data-testid*="confirmationSheetConfirm"]')
+      ),
+      15000
+    );
+    await confirm.click();
+    let url = await driver.getCurrentUrl();
+    assert.equal(url, "https://twitter.com/logout");
   }
   async OpenTwitter() {
     this.page = driver.get("https://twitter.com");
@@ -18,6 +51,8 @@ class LoginPage {
   }
 
   async loginEmail(email) {
+    const logIn = driver.wait(until.elementLocated(By.css('a[href="/login"]')));
+    await logIn.click();
     let emailField = await driver.wait(
       until.elementLocated(By.css("*[name='text']")),
       10000
@@ -34,6 +69,9 @@ class LoginPage {
     await passwordField.sendKeys(password);
     await passwordField.sendKeys(Key.ENTER);
   }
+  async close() {
+    await driver.quit();
+  }
 }
 
-module.exports = LoginPage;
+module.exports = Login;
